f443245877baebee3509afa09031c1b0bd9f9209
feyzahaktanir <feyza.haktanir@hotmail.com>
2022-12-06 16:35:01 +0300

    Selenium Ders 23 - Cucumber

--- src/test/java/stepdefinitions/AmazonStepDefinitions.java
+++ src/test/java/stepdefinitions/AmazonStepDefinitions.java
@@ -2,43 +2,49 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.AmazonPages;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 public class AmazonStepDefinitions {
+
+    AmazonPages amazonPages = new AmazonPages();
+
     @Given("Kullanici Amazon sayfasina gider.")
     public void kullanici_amazon_sayfasina_gider() {
-        Driver.getDriver().get("https://www.amazon.com");
-         throw new io.cucumber.java.PendingException();
+        Driver.getDriver().get(ConfigReader.getProperty("AmazonURL"));
     }
     @Given("Kullanici iPhone icin arama yapar.")
     public void kullanici_i_phone_icin_arama_yapar() {
-
-         throw new io.cucumber.java.PendingException();
+        amazonPages.searchBox.sendKeys("iPhone" + Keys.ENTER);
     }
     @Then("Kullanici sonuclarin iPhone icerdigini test eder.")
     public void kullanici_sonuclarin_i_phone_icerdigini_test_eder() {
+        String resultTR = amazonPages.resultText.getText();
 
-         throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(resultTR.contains("iPhone"));
     }
     @Given("Kullanici tea pot icin arama yapar.")
     public void kullanici_tea_pot_icin_arama_yapar() {
-
-         throw new io.cucumber.java.PendingException();
+        amazonPages.searchBox.sendKeys("tea pot" + Keys.ENTER);
     }
     @Then("Kullanici sonuclarin tea pot icerdigini test eder.")
     public void kullanici_sonuclarin_tea_pot_icerdigini_test_eder() {
+        String resultTR = amazonPages.resultText.getText();
 
-         throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(resultTR.contains("tea pot"));
     }
     @Given("Kullanici flower icin arama yapar.")
     public void kullanici_flower_icin_arama_yapar() {
-
-        throw new io.cucumber.java.PendingException();
+        amazonPages.searchBox.sendKeys("flower" + Keys.ENTER);
     }
     @Then("Kullanici sonuclarin flower icerdigini test eder.")
     public void kullanici_sonuclarin_flower_icerdigini_test_eder() {
+        String resultTR = amazonPages.resultText.getText();
 
-        throw new io.cucumber.java.PendingException();
+        Assert.assertTrue(resultTR.contains("flower"));
     }
 
 }
