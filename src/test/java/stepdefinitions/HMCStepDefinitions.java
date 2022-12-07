2c93a37b8f610384d36cd038d7f5346ac296617f
feyzahaktanir <feyza.haktanir@hotmail.com>
2022-12-07 14:52:54 +0300

    Selenium Ders 23 - Cucumber

---
+++ src/test/java/stepdefinitions/HMCStepDefinitions.java
@@ -0,0 +1,34 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class HMCStepDefinitions {
+
+    @Given("Kullanici {string} linkine tiklar.")
+    public void kullanici_linkine_tiklar(String string) {
+
+
+    }
+    @Then("Kullanici adi olarak {string} girer.")
+    public void kullanici_adi_olarak_girer(String string) {
+
+
+    }
+    @Then("Kullanici sifre olarak {string} girer.")
+    public void kullanici_sifre_olarak_girer(String string) {
+
+
+    }
+    @Then("Kullanici Login butonuna basar.")
+    public void kullanici_login_butonuna_basar() {
+
+
+    }
+    @Then("Kullanici basarili giris yapildigini test eder.")
+    public void kullanici_basarili_giris_yapildigini_test_eder() {
+
+
+    }
+
+}
--- src/test/resources/features/US1005_PositiveLoginTesti.feature
+++ src/test/resources/features/US1005_PositiveLoginTesti.feature
@@ -1,3 +1,11 @@
 Feature: US1005 - Positive Login Testi
+
+  @HMC
   Scenario: TC08 Positive Login
     Given Kullanici "hmcURL" sayfasina gider.
+    And Kullanici "Login" linkine tiklar.
+    Then Kullanici adi olarak "hmcValidUsername" girer.
+    And Kullanici sifre olarak "hmcValidPassword" girer.
+    And Kullanici Login butonuna basar.
+    Then Kullanici basarili giris yapildigini test eder.
+    And Kullanici sayfayi kapatir.
